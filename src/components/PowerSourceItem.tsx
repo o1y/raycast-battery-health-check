51bee1da438fc184148d9971ea7182ab6c8532f8
Oliver Mesieh <olivermesieh@gmail.com>
2022-07-02 18:44:08 +0200

    Show generic power source if detailed data are not available

--- src/components/PowerSourceItem.tsx
+++ src/components/PowerSourceItem.tsx
@@ -3,7 +3,9 @@ import StatsListItem from "./StatsListItem";
 const PowerSourceItem = (props: { isCharging: boolean; adapter?: any }) => {
   const adapterName = props.adapter ? props.adapter["Name"] : "";
   const adapterSerial = props.adapter ? props.adapter["SerialString"] : "";
-  const formatted = props.isCharging === true ? `${adapterName} (${adapterSerial})` : "Battery";
+  const adapterLabel = adapterName && adapterSerial ? `${adapterName} (${adapterSerial})` : 'Power Adapter'
+
+  const formatted = props.isCharging === true ? `${adapterLabel}` : "Battery";
   return <StatsListItem label="Power Source" value={formatted} />;
 };
 
