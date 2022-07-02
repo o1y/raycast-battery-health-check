a1b5f657a4ff6c90d5db160359230835864344b3
Oliver Mesieh <olivermesieh@gmail.com>
2022-07-02 19:10:07 +0200

    Fix power source showing battery indication when no data is available

--- src/components/PowerSourceItem.tsx
+++ src/components/PowerSourceItem.tsx
@@ -4,8 +4,9 @@ const PowerSourceItem = (props: { isCharging: boolean; adapter?: any }) => {
   const adapterName = props.adapter ? props.adapter["Name"] : "";
   const adapterSerial = props.adapter ? props.adapter["SerialString"] : "";
   const adapterLabel = adapterName && adapterSerial ? `${adapterName} (${adapterSerial})` : "Power Adapter";
+  const powerSource = props.isCharging === true ? adapterLabel : "Battery";
 
-  const formatted = props.isCharging === true ? `${adapterLabel}` : "Battery";
+  const formatted = props.isCharging !== undefined ? `${powerSource}` : "--";
   return <StatsListItem label="Power Source" value={formatted} />;
 };
 
