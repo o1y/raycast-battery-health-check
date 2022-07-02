ab66a2d94b52debc55d1495dff044e6b111134b7
Oliver Mesieh <olivermesieh@gmail.com>
2022-07-02 19:02:56 +0200

    Prettify

--- src/components/PowerSourceItem.tsx
+++ src/components/PowerSourceItem.tsx
@@ -3,7 +3,7 @@ import StatsListItem from "./StatsListItem";
 const PowerSourceItem = (props: { isCharging: boolean; adapter?: any }) => {
   const adapterName = props.adapter ? props.adapter["Name"] : "";
   const adapterSerial = props.adapter ? props.adapter["SerialString"] : "";
-  const adapterLabel = adapterName && adapterSerial ? `${adapterName} (${adapterSerial})` : 'Power Adapter'
+  const adapterLabel = adapterName && adapterSerial ? `${adapterName} (${adapterSerial})` : "Power Adapter";
 
   const formatted = props.isCharging === true ? `${adapterLabel}` : "Battery";
   return <StatsListItem label="Power Source" value={formatted} />;
