e01d13a5b898b40aa553098c9f03c563a3486a9e
Oliver Mesieh <olivermesieh@gmail.com>
2022-07-26 14:28:49 +0200

    Round power usage and temperature to two decimal places

--- src/components/TemperatureItem.tsx
+++ src/components/TemperatureItem.tsx
@@ -1,8 +1,8 @@
 import StatsListItem from "./StatsListItem";
 
 const TemperatureItem = (props: { temperature: number }) => {
-  const celcius = Math.round(props.temperature / 100);
-  const fahrenheit = Math.round(celcius * (9 / 5) + 32);
+  const celcius = +(props.temperature / 100).toFixed(2);
+  const fahrenheit = +(celcius * (9 / 5) + 32).toFixed(2);
   const temerature = props.temperature ? `${celcius} °C / ${fahrenheit} °F` : "--";
 
   return <StatsListItem label="Temperature" value={temerature} />;
